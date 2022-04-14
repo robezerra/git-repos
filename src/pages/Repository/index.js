280932fa948ddae281baf88f4284e252fa3ff020
Robert <robertbezerrads@gmail.com>
2022-04-13 22:35:22 -0300

    feat: página de detalhe finalizada

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,17 +1,55 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { FaSpinner, FaArrowLeft } from 'react-icons/fa';
 
-import { Container } from './styles';
+import { Container, Loading, BackButton, Owner } from './styles';
 import api from '../../services/api';
 
 export default function Repository() {
 	const { repository } = useParams();
 
-	console.log(repository);
+	const [repo, setRepo] = useState({});
+	const [issues, setIssues] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		async function load() {
+			const [repositoryData, issuesData] = await Promise.all([
+				api.get(`/repos/${repository}`),
+				api.get(`/repos/${repository}/issues`, {
+					params: {
+						state: 'open',
+						per_page: 5,
+					},
+				}),
+			]);
+
+			setRepo(repositoryData.data);
+			setIssues(issuesData.data);
+			setLoading(false);
+		}
+
+		load();
+	}, []);
+
+	if (loading) {
+		return (
+			<Loading>
+				<FaSpinner color="#FFF" size={50} />
+			</Loading>
+		);
+	}
 
 	return (
 		<Container>
-			<h1>{repository}</h1>
+			<BackButton to="/">
+				<FaArrowLeft color="#000" size={30} />
+			</BackButton>
+			<Owner>
+				<img src={repo.owner.avatar_url} alt={repo.owner.login} />
+				<h1>{repo.name}</h1>
+				<p>{repo.description}</p>
+			</Owner>
 		</Container>
 	);
 }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,3 +1,62 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const Container = styled.div``;
+const animate = keyframes`
+  from {
+    transform: rotate(0deg);
+  } to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+
+	svg {
+		animation: ${animate} 2s linear infinite;
+	}
+`;
+
+export const Container = styled.div`
+	max-width: 700px;
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+	padding: 20px 30px;
+	margin: 80px auto;
+`;
+
+export const Owner = styled.header`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	img {
+		width: 150px;
+		border-radius: 20%;
+		margin-bottom: 20px;
+	}
+
+	h1 {
+		font-size: 30px;
+		color: #0d2636;
+	}
+
+	p {
+		margin-top: 5px;
+		font-size: 14px;
+		color: #000;
+		text-align: center;
+		line-height: 1.4;
+		max-width: 400px;
+	}
+`;
+
+export const BackButton = styled(Link)`
+	border: 0;
+	outline: 0;
+	background: transparent;
+`;
