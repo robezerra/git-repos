198ba3f575c6aed8ed411dcc0dafe2cf11b87796
Robert <robertbezerrads@gmail.com>
2022-04-14 00:03:02 -0300

    feat: paginação implementada

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,7 +2,14 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { FaSpinner, FaArrowLeft } from 'react-icons/fa';
 
-import { Container, Loading, BackButton, Owner } from './styles';
+import {
+	Container,
+	Loading,
+	BackButton,
+	Owner,
+	IssuesList,
+	PageActions,
+} from './styles';
 import api from '../../services/api';
 
 export default function Repository() {
@@ -11,6 +18,7 @@ export default function Repository() {
 	const [repo, setRepo] = useState({});
 	const [issues, setIssues] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [page, setPage] = useState(1);
 
 	useEffect(() => {
 		async function load() {
@@ -32,6 +40,26 @@ export default function Repository() {
 		load();
 	}, []);
 
+	useEffect(() => {
+		async function loadIssue() {
+			const response = await api.get(`/repos/${repository}/issues`, {
+				params: {
+					state: 'open',
+					page: page,
+					per_page: 5,
+				},
+			});
+
+			setIssues(response.data);
+		}
+
+		loadIssue();
+	}, [page, repository]);
+
+	function handlePage(action) {
+		setPage(action === 'back' ? page - 1 : page + 1);
+	}
+
 	if (loading) {
 		return (
 			<Loading>
@@ -50,6 +78,33 @@ export default function Repository() {
 				<h1>{repo.name}</h1>
 				<p>{repo.description}</p>
 			</Owner>
+
+			<IssuesList>
+				{issues.map((issue) => (
+					<li key={issue.id}>
+						<img src={issue.user.avatar_url} alt={issue.user.login} />
+						<div>
+							<strong>
+								<a href={issue.html_url}>{issue.title}</a>
+
+								{issue.labels.map((label) => (
+									<span key={label.id}>{label.name}</span>
+								))}
+							</strong>
+							<p>{issue.user.login}</p>
+						</div>
+					</li>
+				))}
+			</IssuesList>
+
+			<PageActions>
+				<button type="button" onClick={() => handlePage('back')} disabled={page < 2}>
+					Back
+				</button>
+				<button type="button" onClick={() => handlePage('next')}>
+					Next
+				</button>
+			</PageActions>
 		</Container>
 	);
 }
